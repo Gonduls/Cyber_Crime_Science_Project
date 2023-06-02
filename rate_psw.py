d4021fa9247a4d68c8ae4716b3d72ea01f535619
Pierluigi <pierluigi.negro.00@gmail.com>
2023-06-02 19:34:07 +0200

    created rate passwords file

---
+++ rate_psw.py
@@ -0,0 +1,53 @@
+from zxcvbn import zxcvbn
+import sys
+import os
+import threading
+
+# grab path from command line
+path = sys.argv[1]
+
+# grab output file name from command line
+outputFile = sys.argv[2]
+
+# get the list of the files in the first folder
+passwordsFiles = os.listdir(path)
+
+output_list = []
+
+# function that reads all lines of a file and counts the scores amount of each password, then appends results to output_list
+def countScore(file):
+    scores = [0, 0, 0, 0, 0]
+    lines = 0
+    for line in open(path + "/" + file, "r"):
+        lines += 1
+        try:
+            score = zxcvbn(line.strip())["score"]
+        except:
+            continue
+        scores[score] += 1
+    
+    output_list.append((file, scores, lines))
+
+# for file in passwordsFiles create a new thread that does function countScore(file)
+threads = []
+for file in passwordsFiles:
+    t = threading.Thread(target=countScore, args=(file,))
+    t.start()
+    threads.append(t)
+
+# wait for all threads to finish
+for t in threads:
+    t.join()
+
+# sort the output_list by the amount of lines in each file
+output_list.sort(key=lambda x: -x[2])
+total_results = [0, 0, 0, 0, 0]
+
+# write the results to the output file
+with open(outputFile, "w") as f:
+    for file, scores, lines in output_list:
+        f.write(str(lines) + '\t' +file + ": " + str(scores) + "\n")
+        for i in range(5):
+            total_results[i] += scores[i]
+    
+    f.write("Total: " + str(total_results) + "\n")
